89d5647553871545541e4ace30ae13c05cdc58d9
Victhor <victhorgb1080@gmail.com>
2024-05-15 18:48:16 -0300

    incializando

--- serv_web.ino
+++ serv_web.ino
@@ -1,8 +1,22 @@
 
 
+// bibliotecas para conexao wifi e criacao do servidor
+#include <Wifi.h>
+#include <ESPAsyncWebServer.h>
+
+// criacao constantes para a conexao com o wifi
+const char* ssid = "Empresa"; 
+const char* password = "senhafoda";
+
+// cria objeto AsyncWebServer na porta 80
+AsyncWebServer server(80);
+
 void setup() {
   // put your setup code here, to run once:
+  // inicializa com uma faixa de 9600
+  Serial.begin(9600);
 
+  wifi.begin(ssid, passord);
 }
 
 void loop() {
